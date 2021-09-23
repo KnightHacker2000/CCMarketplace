6c3ce78466ae6834b434e9e5a657212f13d77770
yifan <zhu.2278@osu.edu>
2021-09-23 17:41:14 -0400

    Added checkout form page and basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
-
 import { AppComponent } from './app.component';
 import { StockItemComponent } from './stock/stock-item/stock-item.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StockItemComponent
+    StockItemComponent,
+    ContactFormComponent,
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
