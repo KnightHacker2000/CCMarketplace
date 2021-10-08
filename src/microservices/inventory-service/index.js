6b6c7cd5f4e51bffb257e309ae1ab3cba00ce447
yifan <zhu.2278@osu.edu>
2021-10-08 16:31:59 -0400

    changed port to 3000

--- src/microservices/inventory-service/index.js
+++ src/microservices/inventory-service/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 3000;
 
 const routes = require('./api/routes');
 routes(app);
