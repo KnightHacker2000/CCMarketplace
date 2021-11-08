fbd3e0f0e75b91c1120d7d4fe02761ea7a0c4f54
KnightHacker2000 <cheng.1381@osu.edu>
2021-11-07 19:24:33 -0500

    before merging with jiale

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ const orderApp = require('./backend/orderService')
 const payApp = require('./backend/paymentService')
 const shipApp = require('./backend/shipService')
 
-const shipApp = require('./backend/shipService')
-
 const port_stock = process.env.PORT || 3000;
 const port_pay = process.env.PORT || 3001;
 const port_order = process.env.PORT || 8080;
 const port_ship = process.env.PORT || 9000;
-
 
 stockApp.set('port',port_stock);
 orderApp.set('port',port_order);
@@ -25,5 +22,5 @@ const shipServer = http.createServer(shipApp);
 
 orderServer.listen(port_order);
 stockServer.listen(port_stock);
-paymentServer.listen(port_payment);
+paymentServer.listen(port_pay);
 shipServer.listen(port_ship);
